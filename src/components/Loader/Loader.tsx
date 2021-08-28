3b65adecd4db50f2d618bc5df7123bd46e740543
MikeYuen <nhatminh.150596@gmail.com>
2021-08-28 16:12:42 +0700

    TR-14 update color

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+
+import "./Loader.scss";
+
+const Loader: FC = () => {
+  return (
+    <div className="wrapper">
+      <div className="block mover"></div>
+      <div className="block jumper"></div>
+      <div className="block jumper"></div>
+      <div className="block jumper"></div>
+      {/* <div className="block jumper"></div> */}
+    </div>
+  );
+};
+
+export default Loader;
--- src/components/NextHold/NextHold.tsx
+++ src/components/NextHold/NextHold.tsx
@@ -1,8 +1,36 @@
-import React, { FC } from "react";
-import { NextHoldProps } from "./NextHold.types";
+import React, { FC, Fragment } from "react";
+import { NextHoldProps, PreviewPieceProps } from "./NextHold.types";
 
 import "./NextHold.scss";
 import Tile from "../Tile";
+
+const PreviewPiece: FC<PreviewPieceProps> = ({ piece, color }) => {
+  if (piece[0].length === 4 && piece[0][3] === 0 && piece[1][3] === 0) {
+    piece[0].pop();
+    piece[1].pop();
+  }
+  if (piece[0].length === 3 && piece[0][0] === 0 && piece[1][0] === 0) {
+    piece[0].shift();
+    piece[1].shift();
+  }
+  return (
+    <Fragment>
+      {piece.map((line, index) => (
+        <div key={index}>
+          {line.map((item, index) => (
+            <Tile
+              key={index}
+              color={color}
+              filled={!!item}
+              animated={false}
+              size="sm"
+            />
+          ))}
+        </div>
+      ))}
+    </Fragment>
+  );
+};
 
 const NextHold: FC<NextHoldProps> = ({ next, hold }) => {
   const { next: nextPiece, color } = next;
@@ -13,21 +41,11 @@ const NextHold: FC<NextHoldProps> = ({ next, hold }) => {
         <div>Hold</div>
       </div>
       <div className="tr-next-hold__pieces">
-        {/* {JSON.stringify(next)} */}
-        <div>
-          {nextPiece.map((line, index) => (
-            <div key={index}>
-              {line.map((item, index) => (
-                <Tile
-                  key={index}
-                  color={color}
-                  filled={!!item}
-                  animated={false}
-                  size="sm"
-                />
-              ))}
-            </div>
-          ))}
+        <div className="tr-next-hold__next">
+          <PreviewPiece piece={nextPiece} color={color} />
+        </div>
+        <div className="tr-next-hold__hold">
+          <PreviewPiece piece={nextPiece} color={color} />
         </div>
       </div>
     </div>
